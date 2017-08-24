13827f0e17adee23933057da0207b4f5b464ac83
YarikBur <yarikbur@Mail.ru>
2017-08-24 14:36:38 +0300

    INIT

---
+++ desktop/src/ru/sgstudio/burglar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.sgstudio.burglar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.sgstudio.burglar.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
